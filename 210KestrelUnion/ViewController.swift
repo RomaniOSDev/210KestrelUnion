079e6ddfb4852eddc74da4c0e74d26703b28e377
Roman <romazaniosdev@gmail.com>
2026-06-06 21:54:49 +0300

    Initial Commit

---
+++ 210KestrelUnion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  210KestrelUnion
+//
+//  Created by Roman on 6/6/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
